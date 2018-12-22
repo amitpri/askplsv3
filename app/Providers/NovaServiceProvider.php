391de21ad17baa2fe4fddcbe7e9685ee58f6739c
Amit Priyadarshi <amitpri@gmail.com>
2018-12-22 16:59:28 +0530

    error 403 - 3

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -89,11 +89,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
      */
     protected function gate()
     {
- //       Gate::define('viewNova', function ($user) {
-  //          return in_array($user->email, [
-                //
-  //          ]);
-   //     });
+        
     }
 
     /**
