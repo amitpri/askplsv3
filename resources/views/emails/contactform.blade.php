1a8e3d2a35f4b6803df5b1beea314a2e572d2526
Amit Priyadarshi <amitpri@gmail.com>
2019-01-07 21:18:35 +0530

    contact form 3

--- resources/views/emails/contactform.blade.php
+++ resources/views/emails/contactform.blade.php
@@ -15,21 +15,7 @@
                                 <td>
                                     <!-- COPY -->
                                     <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                                        <tr>
-                                            <td align="center" style="font-size: 25px; font-family: Helvetica, Arial, sans-serif; color: #333333; padding-top: 30px;" class="padding"> {{$name}}</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td align="center" style="font-size: 25px; font-family: Helvetica, Arial, sans-serif; color: #333333; padding-top: 30px;" class="padding"> {{$email}}</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td align="center" style="font-size: 25px; font-family: Helvetica, Arial, sans-serif; color: #333333; padding-top: 30px;" class="padding"> {{$phone}}</td>
-                                        </tr>
-                                        
-                                        <tr>
-                                            <td align="center" style="font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: #333333; padding-top: 30px;" class="padding"> {{$message}}</a></td>
-                                        </tr>
+                                      
                                          
                                     </table>
                                 </td>
@@ -41,5 +27,4 @@
         </td>
     </tr> 
 </table>
-</body>
-</html>
+</body> 
