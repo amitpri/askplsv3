938a75fca1c019dee45f22f55fb4a4895ebef1d7
Amit Priyadarshi <amitpri@gmail.com>
2019-01-28 16:44:50 +0530

    schools

--- resources/views/topics.blade.php
+++ resources/views/topics.blade.php
@@ -364,18 +364,29 @@
 
                                 <div class="col-12 col-md-12" v-if="vCatName == 'Schools'"  >
                                     <div class="review-title">
-                                        <h4><a target="_blank" :href="'/c/' +  vCatName  + '/'+ topic.url" style="">@{{ topic.name }}  <h6> @{{ topic.type}}</h6></a></h4>
+                                        <h4><a target="_blank" :href="'/c/' +  vCatName  + '/'+ topic.url" style="">@{{ topic.name }}</a></h4> 
                                     </div>
-                                    <div class="review-content"> 
-                                        <img  v-if="topic.profilepic" :src="'/storage/' + topic.profilepic"  width="100"> 
+                                    <div class="review-content">  
+
+                                      <img  v-if="topic.profilepic" :src="'/storage/' + topic.profilepic"  width="100">  
+                                    
  
                                     </div>
-                                    <ul class="entry-meta clearfix">
-                                        <li><i class="icon-calendar3"></i> @{{ topic.locality }}</li>
-                                        <li> <i class="icon-user"></i> @{{ topic.city }} </li>
-                                        <li><i class="icon-folder-open"></i> @{{ topic.country }} </li>
+                                    <div v-if="topic.profilepic"  class="" style=" ">  
+                                        <p style="font-weight: 400; opacity: 0.8;  " >@{{ topic.address}}    </p>  
+
+                                    </div>
+                                    <div v-else class="" style="margin-top: -20px;">  
+                                        <p style="font-weight: 400; opacity: 0.8;   ">@{{ topic.address}}   </p>  
+
+                                    </div>
+                                     <ul class="entry-meta clearfix">
+                                      
+                                        <li> <i class="icon-user"></i><a href="">@{{ topic.type }}</a>  </li>
+                                        <li><i class="icon-calendar3"></i><a href=""> @{{ topic.locality }}</a></li>
+                                        <li> <i class="icon-user"></i><a href=""> @{{ topic.city }}</a></li> 
                                     
-                                    </ul>
+                                    </ul> 
                                 </div>
                                 
                             </div> 
