0734b355ab759898e9e70c1341e96e7767ca7c8c
Amit Priyadarshi <amitpri@gmail.com>
2019-02-05 08:49:52 +0530

    prices

--- resources/views/prices.blade.php
+++ resources/views/prices.blade.php
@@ -1,34 +1,38 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
-<head>
-
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="author" content="SemiColonWeb" />
+<head> 
 
     <!-- Stylesheets
     ============================================= -->
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="AskPls" />
+
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Playfair+Display:700,700i,900" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
-    <link rel="stylesheet" href="style.css" type="text/css" />
-    <link rel="stylesheet" href="css/dark.css" type="text/css" />
-
-    <!-- Home Demo Specific Stylesheet -->
-    <link rel="stylesheet" href="demos/interior-design/interior-design.css" type="text/css" />
+    <link rel="stylesheet" href="style.css" type="text/css" />  
+    <link rel="stylesheet" href="askpls.css" type="text/css" />
 
     <link rel="stylesheet" href="css/font-icons.css" type="text/css" />
-    <link rel="stylesheet" href="css/animate.css" type="text/css" />
-    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
-
-    <!-- Reader's Blog Demo Specific Fonts -->
-    <link rel="stylesheet" href="demos/interior-design/css/fonts.css" type="text/css" />
+    <link rel="stylesheet" href="css/animate.css" type="text/css" /> 
+ 
+    <link rel="stylesheet" href="askplsfonts.css" type="text/css" />
 
     <link rel="stylesheet" href="css/responsive.css" type="text/css" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="css/colors.php?color=1c85e8" type="text/css" />
 
+    <script src="/vue/vue.min.js"></script>
+    <script src="/axios/axios.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/places.js@1.15.4"></script>
+
+    <link rel="stylesheet" href="css/colors.php?color=FE9603" type="text/css" /> <!-- Theme Color -->
+    <link rel="stylesheet" href="seo/css/fonts.css" type="text/css" />
+    <link rel="stylesheet" href="seo/seo.css" type="text/css" />
+
     <!-- Document Title
     ============================================= -->
-    <title>AskPls | Anonymous Review System</title>
+    <title>AskPls | Anonymous Review System Prices</title>
 
 </head>
 
@@ -106,8 +110,8 @@
                             <!-- Logo
                             ============================================= -->
                             <div id="logo">
-                                <a href="/" class="standard-logo"><img src="images/logo.png" alt="Canvas Logo"></a>
-                                <a href="/" class="retina-logo"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+                                <a href="/" class="standard-logo"><img src="images/logo.png" alt="AskPls"></a>
+                                <a href="/" class="retina-logo"><img src="images/logo@2x.png" alt="AskPls"></a>
                             </div><!-- #logo end -->
 
                         </div>
@@ -118,11 +122,10 @@
                             <nav id="primary-menu" class="with-arrows fnone clearfix">
 
                                 <ul> 
-                                    <li><a href="/about"><div>About AskPls</div></a></li>
-                                    <li><a href="/topics"><div>Topics</div></a></li> 
-                                    <li><a href="/prices"><div>Prices</div></a></li>
-                                    <li><a href="/faqs"><div>FAQs</div></a></li>
-                                    <li><a href="/contact"><div>Contact</div></a></li>
+                                    <li><a href="/t"><div>Home</div></a></li> 
+                                    <li><a href="/about"><div>About AskPls</div></a></li> 
+                                    <li><a href="/prices"><div>Prices</div></a></li> 
+                                    <li><a href="/support"><div>Support</div></a></li>
                                 </ul>
                             </nav><!-- #primary-menu end -->
                         </div>
@@ -165,50 +168,87 @@
         ============================================= -->
         <section id="content" style="margin-top:-50px;">
 
-            <div class="content-wrap notoppadding clearfix">
- 
-                <div class="container topmargin-lg bottommargin-lg clearfix">
-
-                  
-
-                </div> 
-
-                <div class="container clearfix">
-
-                    <div class="emphasis-title center divcenter" style="max-width: 800px">
-                        <h2 class="font-secondary nott t700">1000 of companies using and satisfied...</h2>
-                    </div>
- 
-                    <div class="col_one_third nobottommargin">
-                        <div class="feature-box fbox-small fbox-center fbox-plain fbox-large nobottomborder">
-                            <div class="fbox-icon">
-                                <i class="icon-line2-home"></i>
+            <div class="section m-0" style="background: url('seo/images/sections/4.png') no-repeat center top; background-size: cover; padding: 140px 0 0;">
+                    <div class="container">
+                        <div class="row justify-content-between">
+                            <div class="col-md-4 mt-4">
+                                <div class="heading-block nobottomborder bottommargin-sm">
+                                    <div class="badge badge-pill badge-primary">Pricing Table</div>
+                                     
+                                </div> 
+                                <div class="pricing-tenure-switcher" data-container="#pricing-switch">
+                                    <span class="pts-left">Monthly</span>
+                                    <div class="pts-switcher">
+                                        <div class="switch">
+                                            <input id="switch-toggle-pricing-tenure" class="switch-toggle switch-toggle-round" type="checkbox">
+                                            <label for="switch-toggle-pricing-tenure"></label>
+                                        </div>
+                                    </div>
+                                    <span class="pts-right">Yearly</span>
+                                    <div class="price-discount"></div>
+                                </div>
                             </div>
-                            <h3 class="ls0 t400 nott" style="font-size: 20px;">Simple Pricing</h3>
-                            <p style="font-size: 16px;">Simple pricing based on users.</p>
-                        </div>
-                    </div>
-                    <div class="col_one_third nobottommargin">
-                        <div class="feature-box fbox-small fbox-center fbox-plain fbox-large nobottomborder">
-                            <div class="fbox-icon">
-                                <i class="icon-line2-compass"></i>
+
+                            <div class="col-lg-7 col-md-8" style="margin-top: -100px;">
+                                <div id="section-pricing" class="page-section nopadding nomargin">
+
+                                    <div id="pricing-switch" class="pricing row align-items-end bottommargin-lg clearfix">
+
+                                        <div class="col-md-6">
+
+                                            <div class="pricing-box">
+                                                <div class="pricing-title">
+                                                    <img class="mb-2 nobg noradius" src="seo/images/icons/man.svg" alt="Pricing Icon" width="50">
+                                                    <h3>Free</h3>
+                                                </div> 
+                                                <div class="pricing-features border-bottom-0">
+                                                    <ul>
+                                                        <li><i class="icon-check-circle color mr-2"></i><strong>No</strong> Support</li>
+                                                        <li><i class="icon-check-circle color mr-2"></i><strong>Public</strong> Topics Only</li> 
+                                                    </ul>
+                                                </div>
+                                                <div class="pricing-action">
+                                                    <div class="pts-switch-content-left"><a href="/register" class="button button-rounded button-large  text-dark bg-white border btn-block nott m-0 ls0">Get Started</a></div>
+                                                    <div class="pts-switch-content-right"><a href="/register" class="button button-rounded button-large button-light text-dark bg-white border btn-block nott m-0 ls0">Get Started</a></div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                        <div class="col-md-6">
+
+                                            <div class="pricing-box">
+                                                <div class="pricing-title">
+                                                    <img class="mb-2 nobg noradius" src="seo/images/icons/group.svg" alt="Pricing Icon" width="60">
+                                                    <h3>Paid Plan</h3>  
+                                                </div>
+                                                <div class="pricing-price">
+                                                    <div class="pts-switch-content-left"><span class="price-unit">&dollar;</span>10<span class="price-tenure">Per Month per user</span></div>
+                                                    <div class="pts-switch-content-right"><span class="price-unit">&dollar;</span>75<span class="price-tenure">Per Year</span></div>
+                                                </div>
+                                                <div class="pricing-features border-bottom-0">
+                                                    <ul>
+                                                        <li><i class="icon-check-circle color mr-2"></i><strong>24*7</strong> Support</li>
+                                                        <li><i class="icon-check-circle color mr-2"></i><strong>Public</strong> Topics</li> 
+                                                        <li><i class="icon-check-circle color mr-2"></i><strong>Private</strong> Topics</li> 
+                                                    </ul>
+                                                </div>
+                                                <div class="pricing-action">
+                                                    <div class="pts-switch-content-left"><a href="/registerplanmonthly" class="button button-large button-rounded btn-block capitalize m-0 ls0 button">Get Started</a></div>
+                                                    <div class="pts-switch-content-right"><a href="/registerplanyearly" class="button button-large button-rounded btn-block capitalize m-0 ls0">Get Started</a></div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
                             </div>
-                            <h3 class="ls0 t400 nott" style="font-size: 20px;">Secured Reviews</h3>
-                            <p style="font-size: 16px;">Choose to use your own mail server and keep it complete secure within your premises</p>
-                        </div>
-                    </div>
-                    <div class="col_one_third nobottommargin col_last">
-                        <div class="feature-box fbox-small fbox-center fbox-plain fbox-large nobottomborder">
-                            <div class="fbox-icon">
-                                <i class="icon-line2-directions"></i>
-                            </div>
-                            <h3 class="ls0 t400 nott" style="font-size: 20px;">Cloud or In-premise Setup</h3>
-                            <p style="font-size: 16px;">Choose to get the setup installed at cloud or your in-premise</p>
+
                         </div>
                     </div>
                 </div>
-
-            </div>
 
         </section><!-- #content end -->
 
@@ -250,6 +290,38 @@
     <!-- Footer Scripts
     ============================================= -->
     <script src="js/functions.js"></script>
+
+    <script>
+        //Pricing Table Script
+        jQuery(document).ready( function($){
+            function pricingSwitcher( elementCheck, elementParent, elementPricing ) {
+                elementParent.find('.pts-left,.pts-right').removeClass('pts-switch-active');
+                elementPricing.find('.pts-switch-content-left,.pts-switch-content-right').addClass('hidden');
+
+                if( elementCheck.filter(':checked').length > 0 ) {
+                    elementParent.find('.pts-right').addClass('pts-switch-active');
+                    elementPricing.find('.pts-switch-content-right').removeClass('hidden');
+                } else {
+                    elementParent.find('.pts-left').addClass('pts-switch-active');
+                    elementPricing.find('.pts-switch-content-left').removeClass('hidden');
+                }
+            }
+
+            $('.pts-switcher').each( function(){
+                var element = $(this),
+                    elementCheck = element.find(':checkbox'),
+                    elementParent = $(this).parents('.pricing-tenure-switcher'),
+                    elementPricing = $( elementParent.attr('data-container') );
+
+                pricingSwitcher( elementCheck, elementParent, elementPricing );
+
+                elementCheck.on( 'change', function(){
+                    pricingSwitcher( elementCheck, elementParent, elementPricing );
+                });
+            });
+        });
+
+    </script>
 
 </body>
 </html>
