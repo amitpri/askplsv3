504d8a28fd9ade0b80ce53bec19ff49a205dfc7d
Amit Priyadarshi <amitpri@gmail.com>
2019-02-08 00:15:36 +0530

    payment

--- resources/views/paytm-merchant-form.blade.php
+++ resources/views/paytm-merchant-form.blade.php
@@ -5,12 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Merchant Check Out Page</title>
 </head>
 <body>
 <center><h1>Please do not refresh this page...</h1></center>
 <form method="post" action="{{ $paytm_txn_url }}" name="f1">
     {{ csrf_field()  }}
+    
     <table border="1">
         <tbody>
 		<?php
