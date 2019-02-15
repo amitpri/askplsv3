a7dc410e5dc38b80b81ca98183b0bea927d2769f
Amit Priyadarshi <amitpri@gmail.com>
2019-02-15 15:33:55 +0530

    google changes

--- app/Doctor.php
+++ app/Doctor.php
@@ -15,24 +15,5 @@ class Doctor extends Model
     	return $this->morphMany('App\TopicCategory', 'topicable');
 
     }
-
-    protected static function boot()
-    {
-        parent::boot();
-
-         
-        static::addGlobalScope('user_id', function (Builder $builder) {
-
-            $loggedinid = Auth::user()->id;
-            $loggedinemail = Auth::user()->email;
-
-            if( $loggedinemail != 'amitpri@gmail.com' ){
-                
-                $builder->where('id', '=', $loggedinid);
-
-            }
-            
-
-        });
-    } 
+ 
 }
