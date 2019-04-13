b9bd5d1e52c8a184cfd4bc6a2a9d4cef8ed6f997
Amit Priyadarshi <amitpri@gmail.com>
2019-04-13 13:53:25 +0530

    COmpany

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -127,12 +127,17 @@ if ( $loggedinrole == 'super' ) {
         $loggedinid = Auth::user()->id;
 
         $loggedinrole = Auth::user()->role;
+        $loggedintopicable_type = Auth::user()->topicable_type;
         
         if ( $loggedinrole == 'super' ) {
 
             return 1 === 1;
 
         }elseif( $loggedinrole == 'agent'  ){
+
+            return 1 === 2;
+
+        }elseif( $loggedintopicable_type == 'App\Company'){
 
             return 1 === 2;
 
